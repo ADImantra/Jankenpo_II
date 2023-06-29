0987a1e0b0a98bb5a92d15f9294cf91153df8f3f
ADImantra <C.S.Minty@Gmail.com>
2023-06-28 18:06:24 -0700

    updated js to reflect score on html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,7 @@
 let playerScore = 0;
 let compScore = 0;
 let totalGames = 0;
+
 // creates a random number 1 to 3 and associates it with "rock", 
 // "paper", or "scissors"
 function compTurn() 
@@ -24,9 +25,11 @@ function compTurn()
     } 
 }
 
+// this is actual "game" it takes an input from the player
+// and grabs the computer's choice, then compares them.
 function janKenPo(playerChoice=``) 
 {   // grabs the computer's "play"
-    let compValue = compTurn()
+    let compValue = compTurn();
 
     console.log(`You Chose ${playerChoice}`);
     console.log(`They Chose ${compValue}`);
@@ -35,40 +38,94 @@ function janKenPo(playerChoice=``)
         if (compValue === `scissors`) {
             playerScore++
             totalGames++
+            const currentPlayerScore = document.querySelector("#pScore").innerHTML = `Player Score: ${playerScore}`;
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Win`)
+            checkWinner()
         } else if (compValue === `paper`) {
             compScore++
             totalGames++
+            const currentCompScore = document.querySelector("#cScore").innerHTML = `Comp Score: ${compScore}`;
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Lose`)
+            checkWinner()
         } else {
             totalGames++
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Tie`)
+            checkWinner()
         }
     } else if (playerChoice === `paper`) {
         if (compValue === `rock`) {
             playerScore++
             totalGames++
+            const currentPlayerScore = document.querySelector("#pScore").innerHTML = `Player Score: ${playerScore}`;
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Win`)
+            checkWinner()
         } else if (compValue === `scissors`) {
             compScore++
             totalGames++
+            const currentCompScore = document.querySelector("#cScore").innerHTML = `Comp Score: ${compScore}`;
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Lose`)
+            checkWinner()
         } else {
             totalGames++
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Tie`)
+            checkWinner()
         } 
     } else {
         if (compValue === `paper`) {
             playerScore++
             totalGames++
+            const currentPlayerScore = document.querySelector("#pScore").innerHTML = `Player Score: ${playerScore}`;
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Win`)
+            checkWinner()
         } else if (compValue === `rock`) {
             compScore++
             totalGames++
+            const currentCompScore = document.querySelector("#cScore").innerHTML = `Comp Score: ${compScore}`;
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Lose`)
+            checkWinner()
         } else {
             totalGames++
+            const currentTotalGame = document.querySelector("#gScore").innerHTML = `Total Games: ${totalGames}`;
             console.log(`Tie`)
+            checkWinner()
         }
     }
+}
+
+function checkWinner() 
+{
+    if (playerScore === 5 || compScore === 5) {
+        if (playerScore > compScore) {
+            playerWins()
+        }
+        else if(compScore > playerScore) {
+            compWins()
+        }
+        else {
+            tieGame
+        }
+    } 
+}
+
+function playerWins() 
+{
+    console.log(`You Win`)
+}
+
+function compWins() 
+{
+    console.log(`You Lose`)
+}
+
+function tieGame() 
+{
+    console.log(`It's a Tie`)
 }
